3f0856c6393b98a037627b731f4850904533f82f
Steve Savitzky <steve@savitzky.net>
2016-09-05 22:50:31 -0700

    fix up bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,85 +2,51 @@
 # Bootstrap script for Myrtle
 # sources Honu's bootstrap.sh, then does its own make install-pkgs and make-install.
 
-# The bootstrap process is controlled by the following environment variables:
-# INSTALL_PKGS=true                     -- set to false if you're not in the sudo group
-# HAVE_X=true                           -- set to false if $DISPLAY is undefined
-# REPOS=https://github.com/ssavitzky    -- base for git repositories
+### Overrides:
+
+# Uncomment and edit any of these overrides you need to.
+# If you have your own forks of Honu and MakeStuff you'll want to do something like...
+# REPOS=git@github.com:ssavitzky        # base for git repositories
+
+# It's usually safe to ignore these, because Honu's bootstrap tests for them.
 #
-# For the most part, we can keep
+# INSTALL_PKGS=true                     # set to false if you're not in the sudo group
+# HAVE_X=true                           # set to false to skip X-related packages
+
+## This is where we fetch Honu's bootstrap.sh from:
+#      It's usually ok to fetch it from github even if you've set REPOS.
+HONU=https://github.com/ssavitzky/Honu
 
 
-# HAVE_X will work on systems with X and a fair amount of space; for a
-# tiny system like a Raspberry Pi you'd want to export HAVE_X=false and
-# be selective about which packages you install.
+### Here we go!  Pull down Honu's bootstrap, and source it.
+#
+wget -O honu-bootstrap.sh $HONU/raw/master/bootstrap.sh
+. honu-bootstrap.sh
+#
+###
 
-### Preliminaries:
+### At this point, you can re-bind REPOS and pull down some packages of your own.
+#   Here's where you load your version of Myrtle, plus any extra packages you want
+#   from your own repo.
 
-if [ root = `whoami` ]; then
-    echo "Honu creates many files and directories in your home directory."
-    echo "If you are running as root, you won't be able to edit them."
-    echo "Giving up."
-    exit 1
-fi
+MYRTLE=Myrtle
+EXTRAS=
+MY_REPOS=$REPOS
 
-# Set sensible defaults for INSTALL_PKGS and HAVE_X
-
-if [ -z $INSTALL_PKGS ]; then
-    if groups | grep -q sudo; then
-	INSTALL_PKGS=true	# set false if you don't want to install packages
-    else
-	echo "You don't appear to be in the sudo group, so I won't try to install packages."
-	INSTALL_PKGS=false
-    fi
-fi
-
-if [ -z $HAVE_X ]; then
-    if [ -z $DISPLAY ]; then
-	HAVE_X=false
-    else
-	HAVE_X=true
-    fi
-fi
-
-if $INSTALL_PKGS; then		### Install packages.
-
-    # Required packages: if we don't get these, all is lost.
-    REQUIRED="git curl wget make rsync openssh-client openssh-server"
-
-    # Life is difficult without these, but it's possible.
-    ALMOST_ESSENTIAL="mailutils memtest86+ zile ntp"
-
-    if $HAVE_X; then
-	X_PKGS="emacs git-doc git.el xdu xbase-clients gitk git-gui xterm xttitle"
-    else
-	X_PKGS="emacs-nox"
-    fi
-    sudo apt-get install $REQUIRED $ALMOST_ESSENTIAL $X_PKGS
-
-fi # INSTALL_PKGS
-
-export INSTALL_PKGS HAVE_X
-
-### Now that we have git, we can fetch the repositories we need:
-
-# Prefix for the git repos
-if [ -z "$REPOS" ]; then
-    REPOS=https://github.com/ssavitzky
-fi
 # fetch the repositories we need.
-for r in Honu MakeStuff; do
-    if [ ! -d $$r ]; then git clone $REPOS/$r.git; fi
+for r in $MYRTLE $EXTRAS; do
+    if [ ! -d $$r ]; then git clone $MY_REPOS/$r.git; fi
 done
 
 if $INSTALL_PKGS; then
     # TODO:  look at HAVE_X in the makefiles
-    (cd Honu; make install-pkgs)
+    (cd $MYRTLE; make install-pkgs)
 fi
 
 ### Install the dotfiles and other goodies.
 #   Note that not all of it will be useful or even usable without the
 #   packages, but what's there will give you a comfortable $HOME.
-(cd Honu; make install)
+(cd $MYRTLE; make install)
 
-echo You may need to set up both gnome and xfce power managers.
-echo Home is wherever you carry your shell.  Welcome home, $USER.
+echo "I'm always at home in the seas where I dwell,"
+echo Because home is wherever I carry my \$SHELL.
